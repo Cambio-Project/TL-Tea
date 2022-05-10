7aefaf781c7cc9e70f394a066ad7b08a8712b8f8
LWagner <st148345@stud.uni-stuttgart.de>
2022-05-10 05:48:49 +0200

    Implement Equals for TemporalEventDescription

--- parser/src/main/java/cambio/tltea/parser/core/temporal/TemporalEventDescription.java
+++ parser/src/main/java/cambio/tltea/parser/core/temporal/TemporalEventDescription.java
@@ -1,9 +1,28 @@
 package cambio.tltea.parser.core.temporal;
 
+import java.util.Objects;
+
 public record TemporalEventDescription(String value) implements ITemporalValue {
 
     public String getValue() {
         return value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TemporalEventDescription)) {
+            return false;
+        }
+
+        TemporalEventDescription that = (TemporalEventDescription) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
 }
