f5d4db92eabe83024f5e162d1152d4eb9e2ef644
franksn90 <s.frankausowen@googlemail.com>
2022-10-10 11:51:28 +0200

    Support parsing of multiple formulas for convenience

--- interpreter/src/main/java/cambio/tltea/interpreter/Interpreter.kt
+++ interpreter/src/main/java/cambio/tltea/interpreter/Interpreter.kt
@@ -3,11 +3,50 @@ package cambio.tltea.interpreter
 import cambio.tltea.interpreter.nodes.ConsequenceInterpreter
 import cambio.tltea.parser.core.ASTNode
 import cambio.tltea.parser.mtl.generated.MTLParser
+import cambio.tltea.parser.mtl.generated.ParseException
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
 
 /**
  * @author Lion Wagner
  */
 object Interpreter {
+
+    fun interpretAllAsBehavior(mtlLoc: String, isDebugOn: Boolean = false): List<BehaviorInterpretationResult> {
+        Objects.requireNonNull(mtlLoc)
+        try {
+            if (!Paths.get(mtlLoc).toFile().exists()) {
+                throw FileNotFoundException("[WARNING] Did not find MTL formula file at $mtlLoc")
+            }
+            val lines = Files.readAllLines(Paths.get(mtlLoc))
+            return interpretAllAsBehavior(lines, isDebugOn)
+        } catch (e: IOException) {
+            if (isDebugOn) {
+                e.printStackTrace()
+            }
+        } catch (e: ParseException) {
+            if (isDebugOn) {
+                e.printStackTrace()
+            }
+        }
+        return mutableListOf()
+    }
+
+    fun interpretAllAsBehavior(
+        formulas: Collection<String>,
+        isDebugOn: Boolean = false
+    ): List<BehaviorInterpretationResult> {
+        Objects.requireNonNull(formulas)
+        val results = mutableListOf<BehaviorInterpretationResult>()
+        for (formula in formulas) {
+            val parsed = MTLParser.parse(formula)
+            results.add(interpretAsBehavior(parsed))
+        }
+        return results
+    }
 
     fun interpretAsBehavior(
         mlt_formula: String
